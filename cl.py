719377ae9770cc07b7ea5a99fe92915aab0c5004
Keith Ostertag <keitho@strucktower.com>
2017-12-12 20:05:00 -0500

    now looks for cl_searches.txt for saved searches

--- cl.py
+++ cl.py
@@ -6,12 +6,13 @@ SMTP_PORT = "587"
 SMTP_LOGIN = "jko@fastmail.com"
 SMTP_APP_PASSWORD = "jg44tvgn56qakhm7"
 
-BLACKLIST_FILE = "/home/keith/blacklist.txt"
+SEARCHES_FILE = "/home/keith/cl_searches.txt"   #optionally create this file with one search per line
+BLACKLIST_FILE = "/home/keith/cl_blacklist.txt" # accumulates the results from searches previously run
 
 # point the CL_URL to your local CraigsList by changing 'Louisville' in the following example to your local city:
 CL_URL = "https://louisville.craigslist.org/search/sss?query="
 
-# areas are the craigslist areas nearby that I want to search. NOTE line is continued to two lines using backslash
+# areas are the craigslist nearby areas that I want to search. NOTE this is continued to two lines using backslash
 # Play with CraigsList search "include nearby areas" using the drop-down menus to determine the areas you want to include
 AREAS = "&sort=rel&searchNearby=2&nearbyArea=229&nearbyArea=342&nearbyArea=" \
     "35&nearbyArea=227&nearbyArea=45&nearbyArea=133&nearbyArea=673"
@@ -40,45 +41,59 @@ def mail_it(email_payload):
     connection_object.quit()
 # end of mail_it function #############################################################################################
 
+email = []      # initialize an empty list for use with accumulated/appended fields to go into the notification email
+
 if len(sys.argv) > 1:
     # Get search keys from command line.
     search_keys = '+'.join(sys.argv[1:])
+    searches = []
+    searches.append(search_keys)    # searches list now contains only the one search reprented by the passed arguments on command line
+
 else:
-    # alert for missing parameters then exit
-    print("\nExiting... failed to find search keys/parameters!\n")
-    exit()
-
-request_object = requests.get(CL_URL + search_keys + AREAS)
-request_object.raise_for_status()       # raise an exception for error codes (4xx or 5xx)
-soup = bs4.BeautifulSoup(request_object.text, "lxml")
-
-# blacklist.txt is storage for accumulated craigslist links already seen
-blacklist_file = open(BLACKLIST_FILE, 'a+')    # open for appending and reading but will set pointer to EOF
-blacklist_file.seek(0)  #set file pointer to beginning of file
-blacklist = blacklist_file.readlines()  # create list of pid's contained in BLACKLIST_FILE
-blacklist = [x.strip() for x in blacklist]  # strip out \n' and other whitespace characters
-email = []      # initialize an empty list for use with accumulated/appended fields to go into the notification email
-
-for i in range(len(soup.select('.rows li'))):
-    pid = soup.select('.rows li')[i].get('data-pid')
-    item = soup.select('.rows li')[i].p.a.text
-    try:    # sometimes people don't put a price on their listings!
-        price =soup.select('.rows li')[i].p.find('span', 'result-price').text
+    try:    # if no command line parameters, look for SEARCHES_FILE which should have one search per line
+        f = open(SEARCHES_FILE)
+        searches = f.readlines()
+        searches = [x.strip() for x in searches]
+        searches = list(filter(None, searches))   # in case there's an empty string left over from a trailing \n in file
+        f.close()
     except:
-        price = "Price not found!"
-    link = soup.select('.rows li')[i].p.a.get('href')
+            # alert for missing parameters AND no SEARCHES_FILE found! then exit
+            print("\nExiting... failed to find search keys/parameters! No args and no cl_searches.txt!\n")
+            exit()
 
-    if pid not in blacklist:    # don't want to see items already in the BLACKLIST_FILE
-        print(item + "  " + price)
-        print(pid + "\n")
-        print(link)
-        email.append(item + "  " + price)
-        email.append(link)
-        email.append(pid + "\n")
-        blacklist_file.write(pid + "\n")    # Got these now so put in BLACKLIST_FILE
+for search in searches: # the searches list now contains either the command line search/parameters or the searches from SEARCHES_FILE
+    search_keys = search    # as we iterate thru the searches list
+
+    request_object = requests.get(CL_URL + search_keys + AREAS) # get the CraigsList search results page
+    request_object.raise_for_status()       # raise an exception for error codes (4xx or 5xx)
+    soup = bs4.BeautifulSoup(request_object.text, "lxml")
+
+    # blacklist.txt is storage for accumulated craigslist links already seen
+    blacklist_file = open(BLACKLIST_FILE, 'a+')    # open for appending and reading but will set pointer to EOF
+    blacklist_file.seek(0)  #set file pointer to beginning of file
+    blacklist = blacklist_file.readlines()  # create list of pid's contained in BLACKLIST_FILE
+    blacklist = [x.strip() for x in blacklist]  # strip out \n' and other whitespace characters
 
 
-blacklist_file.close()
+    for i in range(len(soup.select('.rows li'))):
+        pid = soup.select('.rows li')[i].get('data-pid')    # data-pid is the key CraigsList uses to uniquely identify posts
+        item = soup.select('.rows li')[i].p.a.text
+        try:    # sometimes people don't put a price on their listings!
+            price =soup.select('.rows li')[i].p.find('span', 'result-price').text
+        except:
+            price = "Price not found!"
+        link = soup.select('.rows li')[i].p.a.get('href')
+
+        if pid not in blacklist:    # don't want to see items already in the BLACKLIST_FILE
+            print(item + "  " + price)
+            print(pid + "\n")
+            print(link)
+            email.append(item + "  " + price)
+            email.append(link)
+            email.append(pid + "\n")
+            blacklist_file.write(pid + "\n")    # Got these now so put in BLACKLIST_FILE
+
+    blacklist_file.close()
 
 if len(email) > 0:              # If empty no notification needed
     email = '\n'.join(email)    # translate list into a string for email payload
