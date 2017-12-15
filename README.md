ee5263f1fd99c996071554b63b77a877189e4fd6
Keith Ostertag <stuff@card16.com>
2017-12-15 18:40:25 -0500

    added optional -l parameter for local mail only

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-**craigslist_forsale_notify** is a small python3 script (running under Linux on the command line) which aims to replace the Craigs List email notification functionality of 'saved searches' for personal use of the "For Sale" category. This script is designed for and tested on Linux OS only, but can likely be easily modified for other OS's. Support for English only.
+**craigslist_forsale_notify** is a small python3 script (running under Linux from the command line) which aims to replace the Craigs List email notification functionality of 'saved searches' for personal use of the "For Sale" category. This script is designed for and tested on Linux OS only, but can likely be easily modified for other OS's. Support for English only. Probably wouldn't be difficult to modify to search other categories, but I leave that to you.
 
-Since CraigsList does not provide a public API, this script uses scraping to capture information. So this script will need to be modified if CraigsList changes their page/data structure.
+Since CraigsList does not provide a public API, this script uses scraping to capture information. This script will need to be modified if CraigsList changes their page/data structure.
 
 #### Overview of how it works:
-1. You must setup the requirements on your system.
-2. You must edit the script to include the location and nearby areas you want to search, plus your correct SMTP server and email information.
-3. Run the script (from the command line or for use with Crontab) with your search terms as command line arguments (separated by spaces).
+1. You setup the requirements on your system.
+2. You edit the script to include the location and nearby areas you want to search, plus your correct SMTP server and email information.
+3. You run the script (from the command line or  with Crontab) with your search terms as command line arguments (separated by spaces).
 4. The script uses Requests to download the search results page then uses BeautifulSoup to scrape for the pid, item, price, and url of the results.
 5. The script then uses SMTP to send the results to your provided email address.
 6. The script also creates/appends a file which keeps track of the pid's so it won't send you them a second time on future searches.
+7. Optionally- if you set the first commandline parameter to "-l" the script will only email your local user account (on your machine or potentially on your local private network).
 
 #### Benefits:
 1. Run it when and as many times as you like.  By contrast, the CraigsList 'Saved searches' function has an odd algorithm for when it runs, and you have no control.
 2. Won't repeat results as it keeps track of which items you have already seen.  By contrast, the CraigsList 'Saved searches' function will keep sending you the same items.
+3. Optionally- with the "-l" parameter you can receive search results when you don't or can't use your regular Internet email (uses your local system login as sender and recipient). 
 
 
 #### Requirements:
@@ -21,12 +23,11 @@ Since CraigsList does not provide a public API, this script uses scraping to cap
 3. These Python modules properly installed (generally with pip):  
   * requests
   * BeautifulSoup4
-  * lxml.  
-
+  * lxml
 
 4. A path to where the script can write to a file.
 
-#### Example of how I use this script:
+#### Examples of how I use this script:
 There are a few items which appear on CraigsList infrequently that I like to follow over the course of several months. Here is an example of the content of my SEARCHES_FILE which I keep in my home directory:
 
 ```  
@@ -37,14 +38,70 @@ minolta autocord
 hasselblad
 "bay 1 filter"
 ```  
-If I wanted to add a search for, say, concert tickets, I could just do this at the command line:
+If I wanted to add a search to my SEARCHES_FILE for, say, concert tickets, I could just do this at the command line:
 
 ```
 keith@ada:~$ echo \"pink floyd\" \"concert tickets\">> cl_searches.txt
 ```  
-I can run the script manually at anytime, or I can also put it into a crontab for scheduling.
 
-Or just run it from the command line whenever:
+Or just run it manually from the command line whenever:
 ```  
 keith@ada:~/code/cl$ python3 cl.py "pink floyd" "concert tickets"
 ```  
+
+Though typically I just let in run from the crontab once a day.
+
+When I don't want to run an email client or for some reason don't want the results sent to my regular Internet email I just use the  optional "-l" parameter on the comandline and use my system mail, as follows:
+
+```
+keith@ada:~$ python3 cl.py -l "dell latitude i7"
+keith@ada:~$
+keith@ada:~$ mail
+Mail version 8.1.2 01/15/2001.  Type ? for help.
+"/var/mail/keith": 1 message 1 new
+>N  1 keith@localhost    Fri Dec 15 18:11   46/1611  Craig's List notificatio
+& 1
+Message 1:
+From keith@localhost Fri Dec 15 18:11:51 2017
+Envelope-to: keith@localhost
+Delivery-date: Fri, 15 Dec 2017 18:11:51 -0500
+Content-Type: text/plain; charset="utf-8"
+Content-Transfer-Encoding: 7bit
+MIME-Version: 1.0
+Subject: Craig's List notification at 2017-12-15 06:11 PM
+From: keith@localhost
+To: keith@localhost
+Date: Fri, 15 Dec 2017 18:11:51 -0500
+
+Dell Latitude E7440 UltraBook Core i7-16GB-256GB SSD Windows 10 Pro  $225
+https://cincinnati.craigslist.org/ele/d/dell-latitude-e7440-ultrabook/6421223747.html
+6421223747
+
+Laptop Dell latitude E7440, Intel i7, 16gb RAM , 256GB SSD storage.  $360
+https://louisville.craigslist.org/sys/d/laptop-dell-latitude-e7440/6418370643.html
+6418370643
+
+Dell latitude e6430 i7 16 gb ram  $250
+https://indianapolis.craigslist.org/sys/d/dell-latitude-e6430-i7-16-gb/6384630737.html
+6384630737
+
+Dell ultrabook Intel i7 256gb ssd win10 activated  $200
+https://indianapolis.craigslist.org/sys/d/dell-ultrabook-intel-i7-256gb/6420451306.html
+6420451306
+
+Laptops for sale  $50
+https://cincinnati.craigslist.org/sys/d/laptops-for-sale/6410729719.html
+6410729719
+
+Dell Latitude E6530  $450
+https://indianapolis.craigslist.org/sys/d/dell-latitude-e6530/6372361863.html
+6372361863
+
+DELL LATITUDE E6320 LAPTOP i7 CPU  $269
+https://indianapolis.craigslist.org/sys/d/dell-latitude-e6320-laptop-i7/6358155753.html
+6358155753
+
+& d1
+& q
+keith@ada:~$
+```
--- cl.py
+++ cl.py
@@ -1,18 +1,21 @@
-# EDIT THESE constants for your personal use
-# Email parameters- make sure you use the proper login, and you may need to use a dedicated "APP" password. CHECK your email provider!
+# Edit these constants for your personal use
+# SMTP parameters- make sure you use the proper login, and you may need to use a dedicated "APP" password.
+# Check your email provider for documentation.
 # For instance you can find instructions for google mail here: https://support.google.com/accounts/answer/185833?hl=en
-SMTP_SERVER = "smtp.email.com"
+# These first four SMTP variable are NOT needed if you use the -l optional parameter for local mail only
+SMTP_SERVER = "smtp.email_provider.com"
 SMTP_PORT = "587"
-SMTP_LOGIN = "keith@card16.com"
-SMTP_APP_PASSWORD = "xxxxxxxxxxxxxxxx"
+SMTP_LOGIN = "youremailaddress@email_provider.com"
+SMTP_APP_PASSWORD = "xxxxxxxxxxxx"
 
-SEARCHES_FILE = "/home/keith/cl_searches.txt"   # optionally create this file with one search per line
-BLACKLIST_FILE = "/home/keith/cl_blacklist.txt" # file to accumulate the results from searches previously run
+SEARCHES_FILE = "/home/user/cl_searches.txt"   # optionally create this file with one search per line
+BLACKLIST_FILE = "/home/user/cl_blacklist.txt" # file to accumulate the results from searches previously run
 
 # point the CL_URL to your local CraigsList by changing 'Louisville' in the following example to your local city:
 CITY = "Louisville" # CraigsList location, case insensitive
 
-# areas are the craigslist 'nearby areas' that I want to search. NOTE this is continued to two lines using backslash
+# areas are the craigslist 'nearby areas' that I want to search.
+# NOTE this is continued to two lines using backslash
 # Play with CraigsList search "include nearby areas" using the drop-down menus to determine the areas you want to include
 AREAS = "&sort=rel&searchNearby=2&nearbyArea=229&nearbyArea=342&nearbyArea=" \
     "35&nearbyArea=227&nearbyArea=45&nearbyArea=133&nearbyArea=673"
@@ -22,15 +25,12 @@ AREAS = "&sort=rel&searchNearby=2&nearbyArea=229&nearbyArea=342&nearbyArea=" \
 #
 #####################    Nothing below this line needs editing for intended use    ##############
 
-import smtplib, sys
+import smtplib, sys, os
 import requests, bs4
+import smtplib
+import datetime
 
 def mail_it(email_payload):
-    import smtplib
-    import datetime
-
-    # now is the currrent time in my prefered format for an email subject
-    now =datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
 
     connection_object = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     connection_object.ehlo()
@@ -39,6 +39,7 @@ def mail_it(email_payload):
 
     connection_object.sendmail(SMTP_LOGIN, SMTP_LOGIN, 'Subject: CraigsList notification at ' + now + '\n\n' + email_payload.decode('ascii','ignore'))
     connection_object.quit()
+
 # end of mail_it function #############################################################################################
 
 def soup_it():
@@ -47,7 +48,7 @@ def soup_it():
     soup = bs4.BeautifulSoup(request_object.text, "lxml")
 
     # BLACKLIST_FILE is storage for accumulated craigslist links already seen
-    blacklist_file = open(BLACKLIST_FILE, 'a+')    # open for appending and reading but will set pointer to EOF
+    blacklist_file = open(BLACKLIST_FILE, 'a+')
     blacklist_file.seek(0)  #set file pointer to beginning of file
     blacklist = blacklist_file.readlines()  # create list of pid's contained in BLACKLIST_FILE
     blacklist = [x.strip() for x in blacklist]  # strip out \n' and other whitespace characters
@@ -62,28 +63,45 @@ def soup_it():
         link = soup.select('.rows li')[i].p.a.get('href')
 
         if pid not in blacklist:    # don't want to see items already in the BLACKLIST_FILE
-            print(item + "  " + price)  # these three print statements not necessary
-            print(pid + "\n")
-            print(link)
+            # print(item + "  " + price)  # these three print statements not necessary
+            # print(pid + "\n")
+            # print(link)
             email.append(item + "  " + price)
             email.append(link)
             email.append(pid + "\n")
-            blacklist_file.write(pid + "\n")    # Got these now so put in BLACKLIST_FILE
+            blacklist_file.write(pid + "\n")    # Got these now, so put in BLACKLIST_FILE
 
     blacklist_file.close()
-############# end of soup_it function ##########################################################################################
+
+############# end of soup_it function ####################################################################
+
+def local_mail_only(email):
+
+    from email.message import EmailMessage
+
+    msg = EmailMessage()
+    msg.set_content(email)
+    msg['Subject'] = "Craig's List notification at " + now
+    msg['From'] = os.getlogin() + "@localhost"
+    msg['To'] = os.getlogin() + "@localhost"
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit
+
+############ end of local_mail_only function  ##########################################################
 
 cl_url = "https://" + CITY + ".craigslist.org/search/sss?query="
+now =datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
 email = []      # initialize an empty list for scraped results to go into the notification email
 
 if len(sys.argv) > 1:
-    # Get search keys from command line if present.
-    search_keys = '+'.join(sys.argv[1:])
+    # Get search keys from command line if present. Check if optional parameter -l is first
+    search_keys = '+'.join(sys.argv[2:]) if sys.argv[1] == '-l' else '+'.join(sys.argv[1:])
     searches = []
-    searches.append(search_keys)    # searches list now contains only the one search reprented by the passed arguments on command line
+    searches.append(search_keys)    # searches list now contains only the one search represented by the passed arguments on command line
 
 else:
-    try:    # if no command line parameters, look for SEARCHES_FILE which should have one search per line
+    try:    # if no command line parameters, look for SEARCHES_FILE (which should have one search per line)
         f = open(SEARCHES_FILE)
         searches = f.readlines()
         searches = [x.strip() for x in searches]
@@ -100,5 +118,12 @@ for search in searches: # the searches list now contains either the command line
 
 if len(email) > 0:              # If empty no notification needed
     email = '\n'.join(email)    # translate list into a string for email payload
-    mail_it(email.encode('ascii','ignore'))  # send the notification as email by calling the mail_it function
-    # NOTE I'm encoding as ascii here then decoding to unicode inside the mail_it function to deal with stray junk that sometimes happens in CraigsList posts
+
+# send the notification as email by calling the mail_it function
+# OR if -l argument is passed just use local_mail_only function
+# NOTE I'm encoding as ascii here then decoding to unicode inside the mail_it function to deal with stray junk that sometimes happens in CraigsList posts
+
+    if sys.argv[1] == '-l':
+        local_mail_only(email)
+    else:
+        mail_it(email.encode('ascii','ignore'))
